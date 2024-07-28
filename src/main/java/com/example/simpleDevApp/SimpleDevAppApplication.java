a9ed934d09ba87e22bc91f1803263a9b7424a136
akashsawale49 <67323620+akashsawale49@users.noreply.github.com>
2024-07-28 13:36:33 +0530

    firstCommit
    firstCommit

---
+++ src/main/java/com/example/simpleDevApp/SimpleDevAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleDevApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDevAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDevAppApplication.class, args);
+	}
+
+}
